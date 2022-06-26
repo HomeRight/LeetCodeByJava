40495e2c5fdfe832433689cccb10e58818d8b679
HomeRight <alen40817a@gmail.com>
2022-06-26 15:39:38 +0800

    Number_of_1_Bits_191

--- src/main/java/Number_of_1_Bits_191.java
+++ src/main/java/Number_of_1_Bits_191.java
@@ -2,5 +2,24 @@ public class Number_of_1_Bits_191 {
 
     public static void main(String[] args) {
 
+        int n = 00000000000000000000000000001011;
+
+        hammingWeight(n);
+
+
+    }
+
+
+    private static int hammingWeight(int n) {
+        int setBitCount = 0;
+
+        while (n != 0) {
+            n &= (n - 1); // to clear the right most set bit
+            ++setBitCount;
+        }
+
+        System.out.println(setBitCount);
+
+        return setBitCount;
     }
 }
