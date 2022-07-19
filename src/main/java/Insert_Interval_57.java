d3a9954971dd4ae228cdc1e0b38db4c55a5621f6
HomeRight <alen40817a@gmail.com>
2022-07-19 23:27:17 +0800

    update

--- src/main/java/Insert_Interval_57.java
+++ src/main/java/Insert_Interval_57.java
@@ -46,6 +46,7 @@ public class Insert_Interval_57 {
         // insert the last newInterval
         result.add(newInterval);
 
+
         // convert to int[][] array
         System.out.println(result.toArray(new int[result.size()][]));
         System.out.println(Arrays.deepToString(result.toArray(new int[result.size()][])));
